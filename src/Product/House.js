bc462a6c5220353b6f10bb38e14d2b7e4ff58a65
Nicolò Parmiggiani <155823917+Parminico@users.noreply.github.com>
2025-03-11 12:29:30 +0100

    Update House.js

--- src/Product/House.js
+++ src/Product/House.js
@@ -9,7 +9,7 @@ Rispetto ai soliti guanti da 3,5 grammi, qui parliamo di un aumento del 140% di 
 La cosa più sorprendente? La presa. Grazie al grip presente su tutto il palmo, anche gli oggetti più scivolosi restano ben saldi nelle tue mani. E non parliamo solo di attrezzi, ma anche di superfici unte o bagnate. La taglia XL calza perfettamente e il colore arancione li rende super visibili, il che non guasta mai quando lavori in ambienti affollati o poco illuminati.
 
 Se cerchi robustezza e affidabilità, questi guanti sono l’acquisto perfetto. Dimentica i guanti sottili e fragili, qui si gioca in un’altra categoria!`,
-         url: 'https://amzn.to/3XHnX2C'
+         url: 'https://amzn.to/43AFrBC'
      },   
     {
          id: 2,
@@ -21,7 +21,7 @@ Nella confezione ne trovi ben 4, il che è un grande vantaggio: uno per ogni mem
 La vestibilità è comoda e versatile, quindi vanno bene sia sopra un semplice maglietta che sopra un giubbotto pesante. E il bello è che sono leggeri e facili da ripiegare: li butti nel bagagliaio e te ne dimentichi finché non servono.
 
 Se viaggi spesso o vuoi semplicemente essere pronto a tutto, questo set è un’assicurazione di sicurezza a basso costo. Fidati, meglio averli e non usarli che non averli quando servono!`,
-         url: 'https://amzn.to/43AFrBC'
+         url: 'https://amzn.to/3XHnX2C'
      } 
 
 ]
