6db85fa4308733f9793e07fc7a8144715928c7c7
Nicolò Parmiggiani <155823917+Parminico@users.noreply.github.com>
2025-03-11 12:19:18 +0100

    Update House.js

--- src/Product/House.js
+++ src/Product/House.js
@@ -1,11 +1,27 @@
 
 export const house = [
-    // {
-    //     id: 1,
-    //     title: '',
-    //     description: ``,
-    //     url: ''
-    // }   
-    
+    {
+         id: 1,
+         title: 'Giubbotto catarifrangente',
+         description: `Hai presente quando lavori su qualcosa di impegnativo e hai bisogno di un paio di guanti che non ti lascino mai a piedi? Beh, questi guanti in nitrile Diamond da 8,5 grammi sono esattamente quello che ti serve!
+Rispetto ai soliti guanti da 3,5 grammi, qui parliamo di un aumento del 140% di spessore. E questo si traduce in una resistenza incredibile! Li ho provati sia per lavori meccanici sia per il fai-da-te e… niente, nemmeno un graffio o una rottura.
+
+La cosa più sorprendente? La presa. Grazie al grip presente su tutto il palmo, anche gli oggetti più scivolosi restano ben saldi nelle tue mani. E non parliamo solo di attrezzi, ma anche di superfici unte o bagnate. La taglia XL calza perfettamente e il colore arancione li rende super visibili, il che non guasta mai quando lavori in ambienti affollati o poco illuminati.
+
+Se cerchi robustezza e affidabilità, questi guanti sono l’acquisto perfetto. Dimentica i guanti sottili e fragili, qui si gioca in un’altra categoria!`,
+         url: 'https://amzn.to/3XHnX2C'
+     },   
+    {
+         id: 2,
+         title: 'Guanti lattice',
+         description: `Se hai mai avuto un guasto di notte o hai dovuto fermarti sul ciglio della strada, sai quanto sia importante essere ben visibili. Ed è qui che entrano in gioco questi giubbotti catarifrangenti!
+
+Nella confezione ne trovi ben 4, il che è un grande vantaggio: uno per ogni membro della famiglia o anche solo per averne sempre uno di riserva. Realizzati con strisce rifrangenti di alta qualità, questi gilet ti rendono visibile anche nelle condizioni più buie. Li ho provati durante un controllo del veicolo di notte e la differenza è impressionante.
+
+La vestibilità è comoda e versatile, quindi vanno bene sia sopra un semplice maglietta che sopra un giubbotto pesante. E il bello è che sono leggeri e facili da ripiegare: li butti nel bagagliaio e te ne dimentichi finché non servono.
+
+Se viaggi spesso o vuoi semplicemente essere pronto a tutto, questo set è un’assicurazione di sicurezza a basso costo. Fidati, meglio averli e non usarli che non averli quando servono!`,
+         url: 'https://amzn.to/43AFrBC'
+     } 
 
 ]
