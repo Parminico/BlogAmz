d9060e261b8cf1e63869c207c7254a7bcb323c66
Nicolò Parmiggiani <155823917+Parminico@users.noreply.github.com>
2025-03-11 12:21:01 +0100

    Update House.js

--- src/Product/House.js
+++ src/Product/House.js
@@ -2,7 +2,7 @@
 export const house = [
     {
          id: 1,
-         title: 'Giubbotto catarifrangente',
+         title: 'Guanti lattice',
          description: `Hai presente quando lavori su qualcosa di impegnativo e hai bisogno di un paio di guanti che non ti lascino mai a piedi? Beh, questi guanti in nitrile Diamond da 8,5 grammi sono esattamente quello che ti serve!
 Rispetto ai soliti guanti da 3,5 grammi, qui parliamo di un aumento del 140% di spessore. E questo si traduce in una resistenza incredibile! Li ho provati sia per lavori meccanici sia per il fai-da-te e… niente, nemmeno un graffio o una rottura.
 
@@ -13,7 +13,7 @@ Se cerchi robustezza e affidabilità, questi guanti sono l’acquisto perfetto. 
      },   
     {
          id: 2,
-         title: 'Guanti lattice',
+         title: 'Giubbotto catarifrangente',
          description: `Se hai mai avuto un guasto di notte o hai dovuto fermarti sul ciglio della strada, sai quanto sia importante essere ben visibili. Ed è qui che entrano in gioco questi giubbotti catarifrangenti!
 
 Nella confezione ne trovi ben 4, il che è un grande vantaggio: uno per ogni membro della famiglia o anche solo per averne sempre uno di riserva. Realizzati con strisce rifrangenti di alta qualità, questi gilet ti rendono visibile anche nelle condizioni più buie. Li ho provati durante un controllo del veicolo di notte e la differenza è impressionante.
